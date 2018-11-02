11a8730e8b9631347dedd3e38bdfccec45693bd3
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-11-02 18:31:48 -0400

    login register start pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route path="/" component="App"/>
+        </Switch>
+    </Router>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
