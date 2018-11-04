f4de357b47a6916477a0fe2b3c8ff312e27a043d
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-11-04 13:12:01 -0500

    side panel

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Menu, Icon } from 'semantic-ui-react';
+import UserPanel  from '../SidePanel/UserPanel';
 
 class SidePanel extends React.Component{
     render(){
         return(
-            <div>SidePanel</div>
+            <Menu size="large" inverted fixed="left" 
+            vertical style={{ background: '#4c3c4c', fontSize:'1.2rem'}}>
+                <UserPanel/>
+            </Menu>
         )
     }
 }
