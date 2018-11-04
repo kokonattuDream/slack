947ea12fb4b93dbd6ed417a703b28288061dba33
Kerjohn Chen <kokonatsudream@Kerjohns-MacBook-Pro.local>
2018-11-04 14:32:56 -0500

    display user name

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import firebase from '../../firebase';
+import { connect } from 'react-redux';
 import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
 
 class UserPanel extends React.Component{
+    state = {
+        user: this.props.currentUser
+    }
+
     dropdownOptions = () =>[
         {
             key: 'user',
-            text: <span>Signed in as <strong>User</strong></span>,
+            text: <span>Signed in as <strong>{ this.state.user.displayName }</strong></span>,
             disabled: true
         },
         {
@@ -27,6 +32,8 @@ class UserPanel extends React.Component{
     }
 
     render(){
+        
+
         return(
             <Grid style= {{ background: '#4c3c4c '}}>
                 <Grid.Column>
@@ -38,7 +45,7 @@ class UserPanel extends React.Component{
                     </Grid.Row>
 
                     <Header style={{ padding: '0.25em' }} as="h4" inverted>
-                        <Dropdown trigger={<span>User</span>} 
+                        <Dropdown trigger={<span>{ this.state.user.displayName }</span>} 
                             options={ this.dropdownOptions()}/>
                     </Header>
                 </Grid.Column>
@@ -47,4 +54,8 @@ class UserPanel extends React.Component{
     }
 }
 
-export default UserPanel;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(UserPanel);
